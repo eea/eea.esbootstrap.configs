51361fe14f48df3f7e1f816f13e614c5779c3cbf
Anthony Monthe <anthony.monthe@eaudeweb.ro>
2016-12-14 23:14:47 +0100

    Added optional dev behavior

--- docker-start.sh
+++ docker-start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-cd /code/config
-if [ -d ".git" ]; then
-   git pull
-else
-   git clone $github_repo .
-fi 
+set -e
+
+if [[ "${DEV_ENV:-false}" == "true" ]] ; then
+    cd /code/config
+    if [ -d ".git" ]; then
+       git pull
+    else
+       git clone $github_repo . --depth=5
+    fi 
+fi
 chmod 777 /code/config
 chmod -R 775 /code/config/*
