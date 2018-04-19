cbcec7fb3c11c1632b377840a54eb9e54b49bf94
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2018-04-19 12:45:26 +0000

    Refs #94560; Added CO2_passenger_cars app

---
+++ CO2_passenger_cars/public/custom.js
@@ -0,0 +1,18 @@
+/* Add custom js here */
+/* customize esbootstrap options by adding the following global options object
+  window.esbootstrap_options = {
+        initial_search: false,
+        enable_rangeselect: true,
+        enable_geoselect: true
+    }
+ */
+
+// add extra functionalities after the list was displayed or a search was done.
+/* $(window).bind('post_init_callback', function(){
+        customPostInitFunction();
+   });
+
+   $(window).bind('post_search_callback', function(){
+        customPostSearchFunction();
+  });
+*/
