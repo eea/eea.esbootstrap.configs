e618b141e07f692da42934d0b04e8f060406e533
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2020-03-20 16:54:28 +0200

    Refs #114965; Updated query for co2cars

---
+++ CO2_passenger_cars/query.sql
@@ -0,0 +1,61 @@
+This is just a helper file, please update when the query is changed, so next time when testing/modifying directly on discomap, will be easier
+
+Select
+    CONCAT(ID, Status, year) as uid,
+    concat(year,iif(status='F','-09','-03'),'-1') as date,
+    ID,
+    MS,
+    Mp,
+    VFN,
+    Mh,
+    Man,
+    MMS,
+    TAN,
+    T,
+    Va,
+    Ve,
+    Mk,
+    Cn,
+    Ct,
+    Cr,
+    "m (kg)",
+    Mt,
+    iif("Enedc (g/km)" is null, "E (g/km)", "Enedc (g/km)") as "Enedc (g/km)",
+    "Ewltp (g/km)",
+    "W (mm)",
+    "At1 (mm)",
+    "At2 (mm)",
+    "Ft",
+    "Fm",
+    "ec (cm3)",
+    "ep (KW)",
+    "z (Wh/km)",
+    It,
+    iif("Ernedc (g/km)" is null, "Er (g/km)", "Ernedc (g/km)") as "Ernedc (g/km)",
+    "Erwltp (g/km)",
+    "De",
+    "Vf",
+    "r",
+    "year",
+    "Status",
+    "version_file",
+    iif (LOWER(TRIM(ft))='ng-biomethane', 'ng', 
+        iif(LOWER(TRIM(ft))='petrol-electric', 'petrol/electric', 
+            iif(LOWER(TRIM(ft))='diesel-electric', 'diesel/electric',
+                iif(LOWER(TRIM(ft))='hybrid/petrol/e', 'petrol', 
+                    iif(LOWER(TRIM(ft))='petrol phev', 'petrol/electric', 
+                        iif(LOWER(TRIM(ft))='unknown', 'other', 
+                            iif(LOWER(TRIM(ft))='ng_biomethane', 'ng', 
+                                iif(LOWER(TRIM(ft))='petrol-gas', 'ng', 
+                                    iif(LOWER(TRIM(ft))='cng', 'ng', LOWER(TRIM(ft)))
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ) as FtTrim,
+    iif(LOWER(TRIM(status))='f', 'Final', 'Provisional') as scStatus
+
+    from [CO2Emission].[v1r2].[co2cars] WHERE MS!='M1' and ID >= 0 and ID < 10
