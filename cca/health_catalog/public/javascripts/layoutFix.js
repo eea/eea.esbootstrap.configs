803cb89b9b92d3add5f6c632f9e67c4325bc6a9b
Eugen Tripon <tripon.eugen@gmail.com>
2020-06-04 09:54:54 +0300

    Refs #117767 test multiple domain

--- cca/health_catalog/public/javascripts/layoutFix.js
+++
@@ -1,6 +0,0 @@
-// Beacause we use a single external template (with header and footer in the same template)
-// we need this function to insert the search app content into our template
-jQuery(document).ready(function($) {
-  $('#portal-columns').replaceWith($('#portal-columns-app'));
-  $('h2#typeOfData').click();
-});
