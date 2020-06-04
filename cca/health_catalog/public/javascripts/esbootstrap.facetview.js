53600160ba92dd5bd512d2b966b3daeffd0d72fc
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-06-04 17:49:31 +0300

    Improve health catalogue search engine

--- cca/health_catalog/public/javascripts/esbootstrap.facetview.js
+++ cca/health_catalog/public/javascripts/esbootstrap.facetview.js
@@ -1,55 +1,55 @@
 jQuery(document).ready(function($) {
-    if (window.settings_display_images === undefined){
-        settings_display_images = true;
-    }
-    var opts = {
-        search_url: './tools/api',
-        search_index: 'elasticsearch',
-        datatype: 'json',
-        initial_search: false,
-        enable_rangeselect: true,
-        enable_geoselect: true,
-        display_images: settings_display_images,
-        default_sort: [],
-        search_sortby: settings_search_sortby,
-        sort: settings_sort,
-        post_init_callback: function() {
-            // 88482 avoid double add of eea settings and number replacing
-            // since we call also post_search_callback
-            // add_EEA_settings();
-            // replaceNumbers();
-            markNavigationTab(settings_selected_navigation_tab);
-            $(window).trigger('post_init_callback');
-        },
-        post_search_callback: function() {
-            add_EEA_settings();
-            viewReady();
-            replaceNumbers();
-            limitString();
-            $(window).trigger('post_search_callback');
-        },
-        paging: {
-            from: 0,
-            size: 10
-        },
-        display_type_options: settings_display_options,
-        display_type: settings_default_display
-    };
-    if (window.esbootstrap_options) {
-       $.extend(opts, esbootstrap_options);
-    }
-    if ((eea_mapping.highlights !== undefined) && (eea_mapping.highlights.enabled)){
-        opts.highlight_enabled = eea_mapping.highlights.enabled;
-        opts.highlight_whitelist = eea_mapping.highlights.whitelist;
-        opts.highlight_blacklist = eea_mapping.highlights.blacklist;
-    }
-    eea_facetview('.facet-view-simple', opts);
+  if (window.settings_display_images === undefined){
+    settings_display_images = true;
+  }
+  var opts = {
+    search_url: './tools/api',
+    search_index: 'elasticsearch',
+    datatype: 'json',
+    initial_search: false,
+    enable_rangeselect: true,
+    enable_geoselect: true,
+    display_images: settings_display_images,
+    default_sort: [],
+    search_sortby: settings_search_sortby,
+    sort: settings_sort,
+    post_init_callback: function() {
+      // 88482 avoid double add of eea settings and number replacing
+      // since we call also post_search_callback
+      // add_EEA_settings();
+      // replaceNumbers();
+      markNavigationTab(settings_selected_navigation_tab);
+      $(window).trigger('post_init_callback');
+    },
+    post_search_callback: function() {
+      add_EEA_settings();
+      viewReady();
+      replaceNumbers();
+      limitString();
+      $(window).trigger('post_search_callback');
+    },
+    paging: {
+      from: 0,
+      size: 10
+    },
+    display_type_options: settings_display_options,
+    display_type: settings_default_display
+  };
+  if (window.esbootstrap_options) {
+    $.extend(opts, esbootstrap_options);
+  }
+  if ((eea_mapping.highlights !== undefined) && (eea_mapping.highlights.enabled)){
+    opts.highlight_enabled = eea_mapping.highlights.enabled;
+    opts.highlight_whitelist = eea_mapping.highlights.whitelist;
+    opts.highlight_blacklist = eea_mapping.highlights.blacklist;
+  }
+  eea_facetview('.facet-view-simple', opts);
 });
 
 
 function limitString() {
-    $.each($('.tileItem > .tileBody'), function(index, value) {
-      description = $(value).text();
-      $(value).text(description.slice(0, 700) + '...');
-    });
+  $.each($('.tileItem > .tileBody'), function(index, value) {
+    description = $(value).text();
+    $(value).text(description.slice(0, 700) + '...');
+  });
 }
--- cca/health_catalog/public/javascripts/layoutFix.js
+++ cca/health_catalog/public/javascripts/layoutFix.js
@@ -1,6 +1,18 @@
 // Beacause we use a single external template (with header and footer in the same template)
 // we need this function to insert the search app content into our template
 jQuery(document).ready(function($) {
-  $('#portal-columns').replaceWith($('#portal-columns-app'));
+  $(".health-left").remove();
+  $('#content').empty().append($('#portal-columns-app'));
   $('h2#typeOfData').click();
+  // $("img.lazyLoad").Lazy();
+
+  // $(window).on('results_ready', function() {
+  //   console.log('results ready');
+  //     $(".lazyLoad").Lazy();
+  //     $(".lazyload").Lazy();
+  //   if ($.fn.Lazy) {
+  //     $(".lazyLoad").Lazy();
+  //     $(".lazyload").Lazy();
+  //   }
+  // });
 });
