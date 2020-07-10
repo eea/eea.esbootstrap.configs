2b8ffa6c2e203dca1231b6db81dd281d53e813e1
Eugen Tripon <tripon.eugen@gmail.com>
2020-07-10 09:37:32 +0300

    [Refs #117767] set initial items display per page

--- cca/health_catalog/public/javascripts/layoutFix.js
+++ cca/health_catalog/public/javascripts/layoutFix.js
@@ -2,23 +2,36 @@
 // we need this function to insert the search app content into our template
 jQuery(document).ready(function($) {
 
-    setResultsPaging($('#filterDisplayAs span.eea-icon.tabular.selected').length?true:false);
+    setResultsPaging(checkIfTabular());
     $(document).on("mousedown", "#filterDisplayAs span.eea-icon", function(event) {
-        $.fn.facetview.options.paging.size=12;
+        $.fn.facetview.options.paging.size=16;
         if ($(this).attr('class').indexOf('tabular')>0) {
             $.fn.facetview.options.paging.size=1000;
         }
         setResultsPaging($(this).attr('class').indexOf('tabular')>0?true:false);
         //$.fn.facetview.options.paging.size=1000
     });
+
+    function checkIfTabular() {
+        var params = new URLSearchParams(window.location.search);
+        if (!params.has('source')) {
+            return false;
+        }
+        var decodedData = decodeURIComponent(params.get('source'));
+        var jsonObject = JSON.parse(decodedData);
+        return jsonObject.display_type == 'tabular';
+    }
+
     function setResultsPaging(showChart = false) {
         $.fn.facetview.options.paging.size=12;
         if (showChart>0) {
             $.fn.facetview.options.paging.size=1000;
         }
+        console.log('Will display:'+$.fn.facetview.options.paging.size);
     }
     // adjust layout according to "CCA Observatory standard". See
     // https://taskman.eionet.europa.eu/attachments/download/86199/PILOT%20Health%20and%20Climate%20Adaptation%20Observatory_MockUpv4%20cleaned.pdf
+
     var checkExist = setInterval(function() {
         if ($('#content').length) {
             $('#content').empty().append($('#portal-columns-app'));
@@ -26,8 +39,8 @@ jQuery(document).ready(function($) {
         }
     }, 100);
 
-  // /*
-  $('#facetview_rightcol').prepend("<div class='row'>"
+    // /*
+    $('#facetview_rightcol').prepend("<div class='row'>"
                                         +"<div class='column col-md-8' id='filterTitle'><h2>OBSERVATORY RESOURCE CATALOG search tool</h2></div>"
                                         +"<div class='column col-md-4 text-right' id='filterDisplayAs'></div>"
                                     +"</div>"
