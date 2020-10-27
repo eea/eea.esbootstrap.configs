6e5085b68f4a23047261ade1247c3ffeb07c38d9
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2020-10-27 14:52:49 +0100

    Refs #122092; The chemical symbols are displayed nicely in the facets too

--- pam/public/custom.js
+++ pam/public/custom.js
@@ -68,9 +68,42 @@ window.esbootstrap_options = {
     default_sort: default_sort
 };
 
+function updateNumbers(){
+    var possibleContainers = ['.results_text', '.facet_label_text', '.facetview_selection span'];
+    var chemsMapping = {'CH4':'CH<sub>4</sub>',
+                        'CO2':'CO<sub>2</sub>',
+                        'SO2':'SO<sub>2</sub>',
+                        'O3':'O<sub>3</sub>',
+                        'N2O':'N<sub>2</sub>O',
+                        'NO2':'NO<sub>2</sub>',
+                        'NOx':'NO<sub>x</sub>',
+                        'NF3':'NF<sub>3</sub>',
+                        'NH3':'NH<sub>3</sub>',
+                        'C6H6':'C<sub>6</sub>H<sub>6</sub>',
+                        'SF6':'SF<sub>6</sub>'};
+    jQuery.each(possibleContainers, function(idx, container){
+        var elems = jQuery(container);
+        jQuery.each(elems, function(idx, elem){
+            var shouldReplace = false;
+            var replacedText = elem.innerHTML;
+            jQuery.each(chemsMapping, function(key, value){
+                if (replacedText.indexOf(key) !== -1){
+                    replacedText = replacedText.split(key).join(value);
+                    shouldReplace = true;
+                }
+            });
+            if (shouldReplace){
+                elem.innerHTML = replacedText;
+            }
+        });
+    });
+
+}
+
 jQuery(document).ready(function($) {
     $(window).bind('post_search_callback', function(){
         removeMissingDetails();
+        updateNumbers()
         // fixHeights();
     });
 });
