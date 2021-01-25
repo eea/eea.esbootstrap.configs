a1ffd821aef0e58514ed129221f8872832ef30ce
Eugen Tripon <tripon.eugen@gmail.com>
2021-01-25 15:22:28 +0200

    Refs #126816 cca observatory galery for case studies

--- cca/health_catalog/public/javascripts/cca.facetview.js
+++ cca/health_catalog/public/javascripts/cca.facetview.js
@@ -196,6 +196,31 @@ function showArticle(pathName, addLoader = true) {
         $('#facetview').html(doc.getElementById("content-core"));
         $('#facetview').prepend(backButton);
         $(".share-your-info-ace-button").appendTo("#content-core");
+
+        document.getElementById('links').onclick = function (event) {
+          event = event || window.event;
+          var target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {
+              index: link, event: event,
+              onslide: function (index, slide) {
+                self = this;
+                var initializeAdditional = function (index, data, klass, self) {
+                  var text = self.list[index].getAttribute(data),
+                    node = self.container.find(klass);
+                  node.empty();
+                  if (text) {
+                    node[0].appendChild(document.createTextNode(text));
+                  }
+                };
+                initializeAdditional(index, 'data-description', '.description', self);
+                initializeAdditional(index, 'data-copyright', '.casestudies-gallery-copyright', self);
+              }
+            },
+            links = this.getElementsByTagName('a');
+          blueimp.Gallery(links, options);
+        };
+
     });
 }
 
