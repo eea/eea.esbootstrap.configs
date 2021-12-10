c93933600d321409183bc1271b34805963cb60c5
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2021-12-10 16:39:12 +0200

    Fix countries

---
+++ pam/modify.js
@@ -0,0 +1,17 @@
+module.exports = function(doc){
+    const _ = require('underscore');
+    let modified_doc = {}
+    modified_doc = _.extend(modified_doc, doc);
+
+    try{
+      if (doc["Country"] === "Czech Republic"){
+        modified_doc['Country'] = "Czechia"
+      }
+    }
+    catch(err){
+        console.log(err);
+        console.log("Index the document without modifications");
+        modified_doc = doc;
+    }
+    return modified_doc;
+}
