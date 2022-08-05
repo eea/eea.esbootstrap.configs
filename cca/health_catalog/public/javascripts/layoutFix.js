ecf721e0d7b26a861913706c3409c57fb7688869
tripon eugen <tripon.eugen@gmail.com>
2022-08-05 07:07:29 +0300

    Refs #151970 - cca health catalog template language option

--- cca/health_catalog/public/javascripts/layoutFix.js
+++ cca/health_catalog/public/javascripts/layoutFix.js
@@ -2,6 +2,25 @@
 // we need this function to insert the search app content into our template
 
 jQuery(document).ready(function ($) {
+
+  //Move language div if exist in header
+  li = document.querySelectorAll("ul#portal-languageselector > li.currentLanguage");
+  if (li.length) {
+    var clone = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+    clone.id = "cca-lang-menu";
+    clone.className = "dropdown";
+    clone.style.display = "inline-block";
+    var element = document.getElementById('portal-languageselector');
+    clone.innerHTML =
+      '<button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border: 0;background-color: inherit;">'
+          +'<i class="fa fa-globe"></i> '+ li[0].outerText
+          +'<span class="caret"></span>'
+      +'</button>'
+      + element.outerHTML.replace('<ul ', '<ul class="dropdown-menu" style="list-style:none;"');
+    element.replaceWith(clone);
+      $("#cca-lang-menu").appendTo(".menu-nav-item-language-observatory");
+  }
+
   setResultsPaging(checkIfTabular());
 
   window.onload = function () {
