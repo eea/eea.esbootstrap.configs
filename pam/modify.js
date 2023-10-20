921bec0665df67d5261bcb75de089145414ad688
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2023-10-20 15:55:00 +0300

    updated pam for 2023

--- pam/modify.js
+++ pam/modify.js
@@ -1,3 +1,4 @@
+
 module.exports = function(doc){
     const _ = require('underscore');
     let modified_doc = {}
@@ -13,5 +14,26 @@ module.exports = function(doc){
         console.log("Index the document without modifications");
         modified_doc = doc;
     }
+
+    fields = [
+      'GHG_s__affected',
+      'Sector_s__affected',
+      'Objective_s_',
+      'Type_of_policy_instrument',
+      'Status_of_implementation',
+      'Entities_responsible_for_implementing_the_policy__type_',
+      'Is_the_policy_or_measure_related_to_a_Union_policy_',
+      'Related_Union_Policy',
+      'Projection_scenario_in_which_the_policy_or_measure_is_included',
+      'Policy_impacting_EU_ETS__ESD_or_LULUCF_emissions_GHG',
+      'Relevant_union_dimensions_affected'
+    ]
+
+    for (let i = 0; i < fields.length; i++){
+      if (doc[fields[i]] == undefined) {
+        modified_doc[fields[i]] = 'No information';
+      }
+    }
+
     return modified_doc;
 }
--- pam/query.sql
+++ pam/query.sql
@@ -1,1 +1,5 @@
-select * from <TABLE>
+select iif(isnull(Average_expost_emission_reduction__kt_CO2eq_y_GHG, 'No') = 'No', 'No', 'Yes') as Average_expost_emission_reduction__kt_CO2eq_y_GHG_BOOL,
+       iif(isnull(cast(Expost_production_ktoe_year_RES as varchar), 'No') = 'No', 'No', 'Yes') as Expost_production_ktoe_year_RES_BOOL,
+       iif(isnull(cast(Expost_reductions_ktoe_year_final_energy_EE as varchar), 'No') = 'No', 'No', 'Yes') as Expost_reductions_ktoe_year_final_energy_EE_BOOL,
+       concat('https://discomap.eea.europa.eu/map/GHG_PAMS/',Country,'.pdf') as pdf,
+* from <TABLE>
