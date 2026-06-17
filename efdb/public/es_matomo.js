80e0b4a937e841a79c16fc428128d06df26bc46e
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2026-06-17 14:34:51 +0300

    updated templates for efdb app

---
+++ efdb/public/es_matomo.js
@@ -0,0 +1,12 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://matomo.eea.europa.eu/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', false]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+
